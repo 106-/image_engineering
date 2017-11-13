b64a8b1c1d3d927bd504a4d02ddd376435bc8afb
106 <Segmentation-fault@yandex.ua>
2017-11-13 22:46:17 +0900

    fixed refactoring miss

--- report1/main.py
+++ report1/main.py
@@ -7,7 +7,7 @@ sys.path.append("../")
 from gray_scale import gray_scale
 
 def main():
-    img = gray_scale.load_from_raw_image(256, 256, '../poppo.raw')
+    img = gray_scale.load_from_raw(256, 256, '../poppo.raw')
     img.stat()
     img.save_as_pgm('./poppo.pgm')
 
