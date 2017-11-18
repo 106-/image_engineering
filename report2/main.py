51ec28fd3b494cb92b8b1a098bdefedbdb3267a0
106 <Segmentation-fault@yandex.ua>
2017-11-19 04:05:08 +0900

    added comment

--- report2/main.py
+++ report2/main.py
@@ -21,19 +21,23 @@ def average_filter(size):
 def main():
     origin_img = gray_scale.load_from_raw(256, 256, "../poppo.raw")
 
+    # トーンカーブを適用
     gamma = copy.deepcopy(origin_img) 
     gamma.tone_curve(gamma_correction, gamma=0.7 )
     gamma.save_as_pgm("./gamma.pgm")
 
+    # エンボス処理
     emboss = copy.deepcopy(origin_img)
     emboss.emboss(delta=3)
     emboss.save_as_pgm("./emboss.pgm")
 
+    # 平均化フィルタ3x3
     filter_3x3 = average_filter(3)
     avg_3x3 = copy.deepcopy(origin_img)
     avg_3x3.filter(filter_3x3)
     avg_3x3.save_as_pgm("./avg_3x3.pgm")
 
+    # 平均化フィルタ5x5
     filter_5x5 = average_filter(5)
     avg_5x5 = copy.deepcopy(origin_img)
     avg_5x5.filter(filter_5x5)
