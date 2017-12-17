98e7e47b31ddfce5dd27e6d2cbaeb73c99351677
106 <Segmentation-fault@yandex.ua>
2017-12-18 04:34:35 +0900

    行列を使って書き直し

--- interpolation.py
+++ interpolation.py
@@ -2,6 +2,7 @@
 
 from gray_scale import gray_scale
 import math
+from matrix import matrix
 
 def nearest_neighbor(origin_image, origin_coords, trans_coords, default_color=255):
     res_img = gray_scale(height=origin_image.height, width=origin_image.width, default_color=default_color)
@@ -17,20 +18,27 @@ def nearest_neighbor(origin_image, origin_coords, trans_coords, default_color=25
 def bilinear(origin_image, origin_coords, trans_coords, default_color=255):
     res_img = gray_scale(height=origin_image.height, width=origin_image.width, default_color=default_color)
     for orig_coord, trans_coord in zip(origin_coords.values, trans_coords.values):
-        orig_x = orig_coord[0]
-        orig_y = orig_coord[1]
         x = int(trans_coord[0])
         y = int(trans_coord[1])
 
-        # 四方の座標
-        ul = [int(math.floor(orig_y)), int(math.floor(orig_x))] # 左上
-        ur = [int(math.floor(orig_y)), int(math.ceil(orig_x))]  # 右上
-        dl = [int(math.ceil(orig_y)), int(math.floor(orig_x))]  # 左下
-        dr = [int(math.ceil(orig_y)), int(math.ceil(orig_x))]   # 右下
-        if res_img.isin(ul) and res_img.isin(ur) and res_img.isin(dl) and res_img.isin(dr):
-            # ((2つの画素の差)*(座標の小数点以下成分))+(引いたほうの画素)
-            upper_color = (origin_image[ur[0]][ur[1]]-origin_image[ul[0]][ul[1]])*(orig_x-math.floor(orig_x))+origin_image[ul[0]][ul[1]]
-            downer_color = (origin_image[dr[0]][dr[1]]-origin_image[dl[0]][dl[1]])*(orig_x-math.floor(orig_x))+origin_image[dl[0]][dl[1]]
-            color = (downer_color-upper_color)*(orig_y-math.floor(orig_y))+upper_color
-            res_img[y][x] = color
+        orig_x = orig_coord[0]
+        orig_y = orig_coord[1]
+        xf = int(math.floor(orig_x))
+        yf = int(math.floor(orig_y))
+        xc = xf+1 
+        yc = yf+1
+        if res_img.isin([yf,xf]) and res_img.isin([yf,xc]) and res_img.isin([yc,xf]) and res_img.isin([yc,xc]):
+            ma = matrix([
+                [xc-orig_x, orig_x-xf]
+            ])
+            mb = matrix([
+                [origin_image[yf][xf], origin_image[yf][xc]],
+                [origin_image[yc][xf], origin_image[yc][xc]],
+            ])
+            mc = matrix([
+                [yc-orig_y, orig_y-yf]
+            ]).transpose()
+            m = ma*mb*mc
+            color = m.values[0][0] if 0 <= m.values[0][0] < 256 else default_color
+            res_img[y][x] = color 
     return res_img
